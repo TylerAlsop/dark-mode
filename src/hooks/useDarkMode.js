5c8ab7dc08294d8289b157c36d1eaf3c27ca1868
Tyler Alsop <tylerjalsop@gmail.com>
2020-02-18 19:42:06 -0700

    Made an attempt at checking the value from useLocalStorage to be able to add the class dark-mode to the body element. I don't think I have it right. Will ask for help in the standup.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,6 +1,16 @@
+import React, {useEffect} from 'react';
 import useLocalStorage from './useLocalStorage';
 
-export const useDarkMode = () {
-    const [values, setValues] = useLocalStorage([key], initialValues);
+
+export const useDarkMode = (key, initialValues, cb) {
+    const [someValue, setSomeValue] = useLocalStorage(key, initialValues);
+
+    useEffect(() => {
+        const bodyElement = document.getElementsByClassName("body");
+
+        if (window.localStorage.getItem(someValue)) {
+            bodyElement.classList.toggle("dark-mode");
+        }
+    }, ['toggle switch component here']);
 
 };
