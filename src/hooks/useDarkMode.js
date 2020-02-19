43e48b60b736dde824710d48798936946d1daeed
Tyler Alsop <tylerjalsop@gmail.com>
2020-02-18 19:49:08 -0700

    Got help in the standup. I was on the right track but needed to change some things. Added a return to useDarkMode returning an array with the darkMode state.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,15 +2,17 @@ import React, {useEffect} from 'react';
 import useLocalStorage from './useLocalStorage';
 
 
-export const useDarkMode = (key, initialValues, cb) {
-    const [someValue, setSomeValue] = useLocalStorage(key, initialValues);
+export const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage(key);
 
     useEffect(() => {
-        const bodyElement = document.getElementsByClassName("body");
-
-        if (window.localStorage.getItem(someValue)) {
-            bodyElement.classList.toggle("dark-mode");
+        if (darkMode === true) {
+            document.body.classList.add("dark-mode")
+        } else {
+            document.body.classList.remove("dark-mode")
         }
-    }, ['toggle switch component here']);
+    }, [darkMode]);
+
+    return [darkMode, setDarkMode];
 
 };
